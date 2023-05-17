9ea0327772bb8068a0fbf607cd04a1de7b9ab8a3
Guillermo <guillegarzon.gg@gmail.com>
2023-05-17 17:52:57 -0500

    feat: fix create Task

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { Navbar } from '@/components/Navbar';
 import { TaskProvider } from '../context/TasksContext';
 import { Inter } from 'next/font/google';
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <TaskProvider>
+          <Navbar />
           {children}          
         </TaskProvider>
       </body>
